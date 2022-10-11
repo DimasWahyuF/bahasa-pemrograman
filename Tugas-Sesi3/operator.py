613b179a03d08170044e198cbf40fc9cc5e27da8
DimasWahyuF <dimaswahyu0300@student.esaunggul.ac.id>
2022-10-11 21:58:22 +0700

    operator

---
+++ Tugas-Sesi3/operator.py
@@ -0,0 +1,42 @@
+# OPERATOR ARITMATIKA
+
+#Penjumlahan
+print(13 + 2)
+melon = 7
+semangka = 9
+buah = melon + semangka #
+print(buah)
+
+#Pengurangan
+biaya = 20000
+bayar = 10000
+sisabiaya = biaya - bayar
+print("Sisa biaya Anda adalah ", sisabiaya)
+
+#Perkalian
+panjang = 15
+lebar = 8
+tinggi = panjang * lebar
+print(tinggi)
+
+#Pembagian
+seragam = 36
+siswa = 6
+seragamPerAnak = seragam / siswa
+print("Setiap anak akan mendapatkan seragam ", seragamPerAnak)
+
+#Sisa Bagi / Modulus
+bilangan1 = 14
+bilangan2 = 5
+hasil = bilangan1 % bilangan2
+print("Sisa bagi dari bilangan ", bilangan1, " dan ", bilangan2, " adalah ", hasil)
+
+#Pangkat
+bilangan3 = 8
+bilangan4 = 2
+hasilPangkat = bilangan3 ** bilangan4
+print(hasilPangkat)
+
+#Pembagian Bulat
+print(10//3) 
+#10 dibagi 3 adalah 3.3333. Karena dibulatkan maka akan menghasilkan nilai 3
