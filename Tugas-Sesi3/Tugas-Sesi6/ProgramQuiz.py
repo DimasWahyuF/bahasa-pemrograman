db434a2a6ad395c386e3b2d42e8067822199ac40
DimasWahyuF <dimaswahyu0300@student.esaunggul.ac.id>
2022-11-02 21:36:24 +0700

    frish commit

---
+++ Tugas-Sesi3/Tugas-Sesi6/ProgramQuiz.py
@@ -0,0 +1,10 @@
+# Program Quiz
+print("=============")
+print("Program Quiz")
+print("=============")
+
+Nama = input("Masukan Nama :")
+Nim = input("Masukan Nim :")
+
+print("Nama :",Nama)
+print("Nim :",Nim)
